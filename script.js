c692f2b1bc7653dd3c104426fff1834ffb65ddb4
lomuritala <67230691+lomuritala@users.noreply.github.com>
2020-07-19 16:23:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+(function (window){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -67,7 +67,7 @@ for (var i = 0; i < names.length; i++) {
       helloSpeaker.SpeakHello(name);
   }
 }
-//
+})(window);//
 // Display the result array on the web page
 console.log(resultArray);
 document.getElementById("demo").innerHTML = resultArray;
